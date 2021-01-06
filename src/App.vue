ba1d2179d91759055a840b3f73f24fa6d145bc45
kichis <nk.wisteira2@gmail.com>
2021-01-06 22:25:18 +0800

    1.6.2021 close

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,57 @@
 <template>
+  <myheader></myheader>
+
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div>
+    <p v-if="msg.length>0">
+      {{msg}}
+    </p>
+    <p v-else>
+      no-text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import myheader from './components/myheader'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  // name: 'App',
+  // components: {
+  //   // HelloWorld
+  // }
+  components: { 
+    myheader 
+  },
+
+  data(){
+    return{
+      msg: "Hello world!"
+    }
+  },
+
+  methods: {
+    clear () {
+      this.msg = ''
+    }
+  },
+    created () {
+    // fetch('http://www.geonames.org/postalCodeLookupJSON?postalcode=10504&country=US')
+    // .then( response => {
+    //   return response.json()
+    // })
+    // .then( json => {
+    //   this.msg = json.postalcodes[0].adminName1
+    // })
+    // .catch( (err) => {
+    //   this.msg = err // エラー処理
+    // });
   }
+
 }
 </script>
 
