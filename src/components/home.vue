cd9305783d098ec84de529ac96033bce081b2c2e
kichis <nk.wisteira2@gmail.com>
2021-01-14 11:58:32 +0800

    homeのカルーセルスライドの残りは表示エレメントの大きさが決まってから要調整）

--- src/components/home.vue
+++ src/components/home.vue
@@ -14,19 +14,32 @@
                     <div id="homeCarouselControls" class="carousel slide" data-ride="carousel">
                     <!-- オートトランジションをつけるなら data-ride="carousel"> -->
                         <div class="carousel-inner" role="listbox">
-                            <div class="carousel-item active bg-danger justify-content-between">
+                            <div class="carousel-item active justify-content-around bg-danger">
                                 <!-- <img src="" alt="">one -->
-                                    <a href="" class="carouselContent col-lg-2 col-md-6 bg-secondary">one</a>
+                                <div class="col-lg-2 col-md-3">
+                                    <a href="" class="carouselContent  bg-secondary">one</a>
                                     <!-- <p class="num ">one</p> -->
+                                    </div>
                             </div>
-                            <div class="bg-warning carousel-item">
-                                    <a href="" class="carouselContent col-lg-2 col-md-6 bg-success">two</a>
+                            <div class="carousel-item justify-content-around bg-warning">
+                                <div class="col-lg-2 col-md-3">
+                                    <a href="" class="carouselContent col-lg-2 col-md-3  bg-success">two</a>
                                 <!-- <p class="col-lg-2 col-md-6">Two</p> -->
+                                </div>
                                 
                             </div>
-                            <div class="bg-primary carousel-item">
-                                    <a href="" class="carouselContent col-lg-2 col-md-6 bg-dark">three</a>
+                            <div class="carousel-item justify-content-around bg-primary">
+                                <div class="col-lg-2 col-md-3">
+                                    <a href="" class="carouselContent col-lg-2 col-md-3 bg-dark">three</a>
                                 <!-- <p class="col-lg-2 col-md-6">three</p> -->
+                                </div>
+                            </div>
+
+                            <div class="carousel-item justify-content-around">
+                                <div class="col-lg-2 col-md-3">
+                                    <a href="" class="carouselContent col-lg-2 col-md-3 bg-dark">four</a>
+                                <!-- <p class="col-lg-2 col-md-6">three</p> -->
+                                </div>
                             </div>
                         </div>
 
@@ -34,13 +47,14 @@
                             <li data-target="#homeCarouselControls" data-slide-to="0" class="active"></li>
                             <li data-target="#homeCarouselControls" data-slide-to="1"></li>
                             <li data-target="#homeCarouselControls" data-slide-to="2"></li>
+                            <li data-target="#homeCarouselControls" data-slide-to="3"></li>
                         </ol>
 
-                        <a class="carousel-control-prev" href="#homeCarouselControls" role="button" data-slide="prev">
+                        <a class="carousel-control-prev ml-n3" href="#homeCarouselControls" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="sr-only">Previous</span>
                         </a>
-                        <a class="carousel-control-next" href="#homeCarouselControls" role="button" data-slide="next">
+                        <a class="carousel-control-next mr-n3" href="#homeCarouselControls" role="button" data-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="sr-only">Next</span>
                         </a>
@@ -64,10 +78,6 @@
 
 /* #myImage{
     float: right;
-} */
-
-/* #carouselExampleControls{
-    color: black;
 } */
 
 /* コントローラー矢印を白から黒に変更 */
@@ -107,32 +117,31 @@
 
 /* large - display 6 */
 @media (min-width: 992px) {
-    
+
     .carousel-inner .carousel-item-right.active,
     .carousel-inner .carousel-item-next {
       transform: translateX(50%);
-    }
-    
+    } 
     .carousel-inner .carousel-item-left.active, 
     .carousel-inner .carousel-item-prev {
-      transform: translateX(-50%);
-    }
+      transform: translateX(-20%);
+    }  
+
 }
 
 .carousel-inner .carousel-item-right,
 .carousel-inner .carousel-item-left{ 
     transform: translateX(0);
 }
+
+.carousel-inner{
+    width: 1000px;
+}
 .carouselContent{
-    width: 300px;
+    /* width: 300px; */
     height: 200px;
 }
-/* .movie-img {
-	height:200px;width: 150px;background-color:  aqua;overflow: hidden;background-image: url("https://i.hizliresim.com/mX07R4.png");
-	background-repeat:no-repeat;
-	background-size: cover;
-	object-fit: fill;
-} */
+
 </style>
 
 <script>
