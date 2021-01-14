ddf023cef8a734a5664bf44374cb9425245af62f
kichis <nk.wisteira2@gmail.com>
2021-01-14 14:03:24 +0800

    skillsコンポーネントの大枠完了

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -1,3 +1,51 @@
 <template>
-    <h1>Skillsです</h1>
+    <h2>Skills</h2>
+
+    <div class="row">
+        <div class="col-5 container">
+            <div>
+                <h5>■IT</h5>
+                <p>languages:</p>
+                <p>
+                    <ul>
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>JavaScript</li>
+                        <li>Vue</li>
+                        <li>React</li>
+                        <li>PHP</li>
+                        <li>Laravel</li>
+                        <li>C#</li>
+                    </ul>
+                </p>
+
+                <p>team develpment:</p>
+                <p>
+                    <ul>
+                        <li>通っていたスクールの仲間とチーム開発を行いました</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+
+        <div class="col-5 container">
+            
+            <div>
+                <h5>■Business</h5>
+                <p>前職で得た経験</p>
+            </div>
+
+            <div>
+                <h5>■Others</h5>
+                <p>所持資格</p>
+                <p>
+                    <ul>
+                        <li>TOEIC 740点</li>
+                        <li>ビジネス会計検定C級</li>
+                    </ul>
+                </p>
+            </div>
+        
+        </div>
+    </div> 
 </template>
