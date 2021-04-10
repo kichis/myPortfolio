b4a757b1f72e82568dea556e5ccb958014dbc815
kichis <nk.wisteira2@gmail.com>
2021-04-10 13:59:47 +0800

    main.js、App.vueの不要箇所削除

--- src/main.js
+++ src/main.js
@@ -2,13 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-
 const app = createApp(App)
 app.use(router).mount('#app')
-
-// createApp({
-
-//     // ここに各種設定
-
-// }).mount('#app');
-
